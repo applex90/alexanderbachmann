705d2d7ad63f9f0eb3a71bdb853966f3890be7d3
applex90 <alex20bachmann@gmx.de>
2022-06-04 04:30:51 +0200

    added home-section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeSectionComponent } from './home-section/home-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MainComponent,
+    FooterComponent,
+    HomeSectionComponent
   ],
   imports: [
     BrowserModule,
