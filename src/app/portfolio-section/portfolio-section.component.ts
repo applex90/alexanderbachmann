fa415c8f724614ca6755b7bdd97b99381bd42312
applex90 <alex20bachmann@gmx.de>
2022-06-15 18:47:42 +0200

    changed template for portfolio to ngFor

--- src/app/portfolio-section/portfolio-section.component.ts
+++ src/app/portfolio-section/portfolio-section.component.ts
@@ -5,7 +5,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './portfolio-section.component.html',
   styleUrls: ['./portfolio-section.component.scss']
 })
+
 export class PortfolioSectionComponent implements OnInit {
+  projects = [
+    { title: 'Canvas Game', description: 'El Pollo Loco is an object-oriented jump-and-run-game based on JavaScript.', imgsrc: 'assets/img/el_pollo_loco.jpg', alt: 'el_pollo_loco' },
+    { title: 'Quiz-App', description: 'A simple quiz app that tests your general knowledge of HTML, CSS and JavaScript.', imgsrc: 'assets/img/quizapp.jpg', alt: 'quizapp' },
+  ];
+
 
   constructor() { }
 
