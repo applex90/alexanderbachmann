6d90417f7ee09052138a7b02b95df07a828a2745
applex90 <alex20bachmann@gmx.de>
2022-06-22 21:02:20 +0200

    added url for projects

--- src/app/portfolio-section/portfolio-section.component.ts
+++ src/app/portfolio-section/portfolio-section.component.ts
@@ -8,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 
 export class PortfolioSectionComponent implements OnInit {
   projects = [
-    { title: 'Canvas Game', description: 'El Pollo Loco is an object-oriented jump-and-run-game based on JavaScript.', imgsrc: 'assets/img/el_pollo_loco.jpg', alt: 'el_pollo_loco' },
-    { title: 'Quiz-App', description: 'A simple quiz app that tests your general knowledge of HTML, CSS and JavaScript.', imgsrc: 'assets/img/quizapp.jpg', alt: 'quizapp' },
-    { title: 'Ring of Fire', description: 'The popular drinking game as multi-user-app based on Angular and Firebase.', imgsrc: 'assets/img/ringoffire.jpg', alt: 'Ring of Fire' }
+    { title: 'Canvas Game', description: 'El Pollo Loco is an object-oriented jump-and-run-game based on JavaScript.', imgsrc: 'assets/img/el_pollo_loco.jpg', alt: 'el_pollo_loco', url: 'https://www.alexander-bachmann-portfolio.de/canvas_game' },
+    { title: 'Quiz-App', description: 'A simple quiz app that tests your general knowledge of HTML, CSS and JavaScript.', imgsrc: 'assets/img/quizapp.jpg', alt: 'quizapp', url: 'https://www.alexander-bachmann-portfolio.de/quiz_app' },
+    { title: 'Ring of Fire', description: 'The popular drinking game as multi-user-app based on Angular and Firebase.', imgsrc: 'assets/img/ringoffire.jpg', alt: 'Ring of Fire', url: 'https://ring-of-fire-e4f0f.web.app/' }
   ];
 
 
