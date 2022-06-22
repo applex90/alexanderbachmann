0b617e0ac07e4fe5033bec99b00d73e03f8ba36e
applex90 <alex20bachmann@gmx.de>
2022-06-22 20:51:58 +0200

    added ring of fire project

--- src/app/portfolio-section/portfolio-section.component.ts
+++ src/app/portfolio-section/portfolio-section.component.ts
@@ -10,6 +10,7 @@ export class PortfolioSectionComponent implements OnInit {
   projects = [
     { title: 'Canvas Game', description: 'El Pollo Loco is an object-oriented jump-and-run-game based on JavaScript.', imgsrc: 'assets/img/el_pollo_loco.jpg', alt: 'el_pollo_loco' },
     { title: 'Quiz-App', description: 'A simple quiz app that tests your general knowledge of HTML, CSS and JavaScript.', imgsrc: 'assets/img/quizapp.jpg', alt: 'quizapp' },
+    { title: 'Ring of Fire', description: 'The popular drinking game as multi-user-app based on Angular and Firebase.', imgsrc: 'assets/img/ringoffire.jpg', alt: 'Ring of Fire' }
   ];
 
 
