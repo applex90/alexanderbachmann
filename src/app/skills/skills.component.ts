d79f94b8b6c5ff53133fff72824fb785431a271b
applex90 <alex20bachmann@gmx.de>
2022-06-24 21:40:50 +0200

    added skills and changed menu styling and added git logo to footer

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent implements OnInit {
-
+  skills = [
+    {title: 'HTML / CSS', imgname: 'html_css'},
+    {title: 'SCSS', imgname: 'scss'},
+    {title: 'JavaScript', imgname: 'js'},
+    {title: 'PHP', imgname: 'php'},
+    {title: 'Angular', imgname: 'angular'},
+    {title: 'Application Programming Interface', imgname: 'api'},
+    {title: 'Bootstrap', imgname: 'bootstrap'},
+    {title: 'Scrum', imgname: 'scrum'},
+    {title: 'Design Thinking', imgname: 'design'},
+    {title: 'DBMS / DB', imgname: 'db'},
+    {title: 'Git', imgname: 'git'},
+    {title: 'Test Automation', imgname: 'test'}
+  ]
   constructor() { }
 
   ngOnInit(): void {
