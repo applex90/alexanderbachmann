03d9d360c0c4e3e9835da6044dfc50b757687e3d
applex90 <alex20bachmann@gmx.de>
2022-09-01 18:39:23 +0200

    changed transition time and added new project with empty content

--- src/app/portfolio-section/portfolio-section.component.ts
+++ src/app/portfolio-section/portfolio-section.component.ts
@@ -10,7 +10,8 @@ export class PortfolioSectionComponent implements OnInit {
   projects = [
     { title: 'Canvas Game', description: 'El Pollo Loco is an object-oriented jump-and-run-game based on JavaScript.', imgsrc: 'assets/img/el_pollo_loco.jpg', alt: 'el_pollo_loco', url: 'https://www.alexander-bachmann-portfolio.de/canvas_game' },
     { title: 'Quiz-App', description: 'A simple quiz app that tests your general knowledge of HTML, CSS and JavaScript.', imgsrc: 'assets/img/quizapp.jpg', alt: 'quizapp', url: 'https://www.alexander-bachmann-portfolio.de/quiz_app' },
-    { title: 'Ring of Fire', description: 'The popular drinking game as multi-user-app based on Angular and Firebase.', imgsrc: 'assets/img/ringoffire.jpg', alt: 'Ring of Fire', url: 'https://ring-of-fire-e4f0f.web.app/' }
+    { title: 'Ring of Fire', description: 'The popular drinking game as multi-user-app based on Angular and Firebase.', imgsrc: 'assets/img/ringoffire.jpg', alt: 'Ring of Fire', url: 'https://ring-of-fire-e4f0f.web.app/' },
+    { title: 'Slackclone', description: 'A Slack chat app clone made with Angular and Firebase.', imgsrc: 'assets/img/ringoffire.jpg', alt: 'Slackclone', url: '' }
   ];
 
 
